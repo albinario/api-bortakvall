40836cb9550bbd2cc44aa8cb059128460e243067
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-01-28 14:05:48 +0100

    cleanup fnuttar

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -9,27 +9,27 @@ router.get('/:productId', show)
 
 router.post('/', [
 	body('name')
-		.exists().withMessage('Name is missing').bail()
-		.isString().withMessage('Has to be a string')
-		.isLength({ min: 2, max: 191 }).withMessage('String has to be between 2 and 191 characters'),
+		.exists().withMessage("Name is missing").bail()
+		.isString().withMessage("Name has to be a string")
+		.isLength({ min: 2, max: 191 }).withMessage("Name has to be between 2 and 191 characters"),
 	body('description')
-		.exists().withMessage('Description is missing').bail()
-		.isString().withMessage('Has to be a string'),
+		.exists().withMessage("Description is missing").bail()
+		.isString().withMessage("Description has to be a string"),
 	body('price')
-		.exists().withMessage('Price is missing').bail()
-		.isInt({ min: 1 }).withMessage('Has to be an integer with a value'),
+		.exists().withMessage("Price is missing").bail()
+		.isInt({ min: 1 }).withMessage("Price has to be an integer with a value"),
 	body('images')
 		.optional()
-		.isObject().withMessage('Has to be an object'),
+		.isObject().withMessage("Images has to be an object"),
 	body('stock_status')
-		.exists().withMessage('Stock status is missing').bail()
-		.isString().withMessage('Has to be a string'),
+		.exists().withMessage("Stock status is missing").bail()
+		.isString().withMessage("Stock status has to be a string"),
 	body('stock_quantity')
-		.exists().withMessage('Stock quantity is missing').bail()
-		.isInt({ min: 0 }).withMessage('Has to be a positive integer'),
+		.exists().withMessage("Stock quantity is missing").bail()
+		.isInt({ min: 0 }).withMessage("Stock quantity has to be a positive integer"),
 	body('on_sale')
 		.optional()
-		.isBoolean().withMessage('Has to be a boolean')
+		.isBoolean().withMessage("On sale has to be a boolean")
 ], store)
 
 export default router
