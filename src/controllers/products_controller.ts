dc01374f75e9ea724a676004785cc092a3517f70
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-01-28 13:15:27 +0100

    add product store function with validation

--- src/controllers/products_controller.ts
+++ src/controllers/products_controller.ts
@@ -14,7 +14,7 @@ export const index = async (req: Request, res: Response) => {
 		})
 	} catch (err) {
 		res.status(500).send({
-			status: "fail",
+			status: "error",
 			message: "Something went wrong"
 		})
 	}
@@ -33,11 +33,35 @@ export const show = async (req: Request, res: Response) => {
 		})
 	} catch (err) {
 		return res.status(404).send({
-			status: "error",
+			status: "fail",
 			message: "Not found"
 		})
 	}
 }
 
 export const store = async (req: Request, res: Response) => {
+	const validationErrors = validationResult(req)
+	if (!validationErrors.isEmpty()) {
+		return res.status(400).json({ status: "fail", data: validationErrors.array() })
+	}
+
+	try {
+		const product = await prisma.product.create({
+			data: {
+				name: req.body.name,
+				description: req.body.description,
+				price: req.body.price,
+				on_sale: req.body.on_sale,
+				images: req.body.images,
+				stock_status: req.body.stock_status,
+				stock_quantity: req.body.stock_quantity
+			}
+		})
+		res.send({
+			status: "success",
+			data: product
+		})
+	} catch (err) {
+		return res.status(500).send({ status: "error", message: "Something went wrong" })
+	}
 }
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -7,6 +7,29 @@ router.get('/', index)
 
 router.get('/:productId', show)
 
-router.post('/', [], store)
+router.post('/', [
+	body('name')
+		.exists().withMessage('Name is missing').bail()
+		.isString().withMessage('Has to be a string')
+		.isLength({ min: 2, max: 191 }).withMessage('String has to be between 2 and 191 characters'),
+	body('description')
+		.exists().withMessage('Description is missing').bail()
+		.isString().withMessage('Has to be a string'),
+	body('price')
+		.exists().withMessage('Price is missing').bail()
+		.isInt({ min: 1 }).withMessage('Has to be an integer with a value'),
+	body('images')
+		.optional()
+		.isObject().withMessage('Has to be an object'),
+	body('stock_status')
+		.exists().withMessage('Stock status is missing').bail()
+		.isString().withMessage('Has to be a string'),
+	body('stock_quantity')
+		.exists().withMessage('Stock quantity is missing').bail()
+		.isInt({ min: 0 }).withMessage('Has to be a positive integer'),
+	body('on_sale')
+		.optional()
+		.isBoolean().withMessage('Has to be a boolean')
+], store)
 
 export default router
